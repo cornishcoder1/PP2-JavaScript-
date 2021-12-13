d4f5c27e4d746ec8432ebc5b4170aa8cc4e0d3a2
cornishcoder1 <leahfisher07@hotmail.co.uk>
2021-12-13 14:53:35 +0000

    add questions array

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,3 +3,15 @@ const choices = Array.from(document.getElementsByClassName('choice-text'));
 const progressText = document.getElementById('progressText');
 const scoreText = document.getElementById('score');
 const progressBarFull = document.getElementById('progressBarFull');
+
+let currentQuestion = {};
+let acceptingAnswers = true;
+let score = 0;
+let questionCounter = 0;
+let availableQuestions = [];
+
+let questions = [
+    {
+        question:
+    }
+]
