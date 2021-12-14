fd8d1803264142ef2ad4ef28d4e88ec048d05ba7
cornishcoder1 <leahfisher07@hotmail.co.uk>
2021-12-14 13:17:02 +0000

    Change .correct and .incorrect class colors

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,8 @@
-const question = document.querySelector('#question');
-const choices = Array.from(document.querySelectorAll('.choice-text'));
-const progressText = document.querySelector('#progressText');
-const scoreText = document.querySelector('#score');
-const progressBarFull = document.querySelector('#progressBarFull');
+const question = document.getElementById('question');
+const choices = Array.from(document.getElementsByClassName('choice-text'));
+const progressText = document.getElementById('progressText');
+const scoreText = document.getElementById('score');
+const progressBarFull = document.getElementById('progressBarFull');
 
 let currentQuestion = {}
 let acceptingAnswers = true
@@ -99,7 +99,7 @@ let questions = [
 ]
 
 const SCORE_POINTS = 100
-const MAX_QUESTIONS = 4
+const MAX_QUESTIONS = 10
 
 startGame = () => {
     questionCounter = 0
